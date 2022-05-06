2a9d757cbc5a62a16a1de8e9a6f7cd2dd79635c7
Irina Marchuk <mar4ukira@gmail.com>
2022-05-06 12:04:37 +0100

    added promtail input operator

---
+++ operator/input/promtail/promtail.go
@@ -0,0 +1,50 @@
+package promtail
+
+import (
+	"github.com/grafana/loki/clients/pkg/promtail/positions"
+	"github.com/grafana/loki/clients/pkg/promtail/scrapeconfig"
+	"github.com/grafana/loki/clients/pkg/promtail/targets/file"
+	"go.uber.org/zap"
+
+	"github.com/open-telemetry/opentelemetry-log-collection/operator"
+	"github.com/open-telemetry/opentelemetry-log-collection/operator/helper"
+)
+
+func init() {
+	operator.Register("promtail_input", func() operator.Builder { return NewPromtailInputConfig("") })
+}
+
+func NewPromtailInputConfig(operatorID string) *PromtailInputConfig {
+	return &PromtailInputConfig{
+		InputConfig: helper.NewInputConfig(operatorID, "promtail_input"),
+	}
+}
+
+// PromtailInputConfig is the configuration of a journald input operator
+type PromtailInputConfig struct {
+	helper.InputConfig `mapstructure:",squash" yaml:",inline"`
+	PromtailConfig          *PromtailConfig `mapstructure:"-"`
+	ConfigPlaceholder       interface{}     `mapstructure:"config"`
+}
+
+type PromtailConfig struct {
+	PositionsConfig positions.Config      `yaml:"positions,omitempty"`
+	ScrapeConfig    []scrapeconfig.Config `yaml:"scrape_configs,omitempty"`
+	TargetConfig    file.Config           `yaml:"target_config,omitempty"`
+}
+
+// Build will build a promtail input operator from the supplied configuration
+func (c PromtailInputConfig) Build(logger *zap.SugaredLogger) (operator.Operator, error) {
+	inputOperator, err := c.InputConfig.Build(logger)
+	if err != nil {
+		return nil, err
+	}
+
+	return &PromtailInput{
+		InputOperator: inputOperator,
+	}, nil
+}
+
+type PromtailInput struct {
+	helper.InputOperator
+}
